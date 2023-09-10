ded42e822b061796a5567b9819141df740ee5b7b
Hugo Otsuka <hugo.otsuka@gmail.com>
2023-09-10 17:39:44 +0900

    Add StakeToken

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
 import { NextPage } from "next";
-import { Container, Heading } from "@chakra-ui/react";
+import { Container, Flex, Heading } from "@chakra-ui/react";
+import { useAddress } from "@thirdweb-dev/react";
+import StakeToken from "../components/StakeToken";
 
 const Home: NextPage = () => {
-  return <Container></Container>;
+  const address = useAddress();
+
+  if (!address) {
+    return (
+      <Container maxW={"1200px"}>
+        <Flex h={"50vh"} justifyContent={"center"} alignItems={"center"}>
+          <Heading>Please Connect a Wallet</Heading>
+        </Flex>
+      </Container>
+    );
+  }
+
+  return (
+    <Container maxW={"1200px"}>
+      <StakeToken />
+    </Container>
+  );
 };
 
 export default Home;
